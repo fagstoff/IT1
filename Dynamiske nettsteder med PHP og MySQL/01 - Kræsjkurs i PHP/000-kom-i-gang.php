0b765492c5d310fd667840b2934d040929bddcd4
bitjungle <devel@bitjungle.com>
2015-05-24 15:24:31 +0200

    Utkast til en introduksjonstekst

--- Dynamiske nettsteder med PHP og MySQL/01 - Kræsjkurs i PHP/000-kom-i-gang.php
+++ Dynamiske nettsteder med PHP og MySQL/01 - Kræsjkurs i PHP/000-kom-i-gang.php
@@ -2,6 +2,11 @@
 //Copyright 2015 BITJUNGLE Rune Mathisen
 //Lisens: https://www.apache.org/licenses/LICENSE-2.0
 
+//Velkommen til dette kræsjkurset i PHP. Målet med kurset er å gi deg en helt
+//grunnleggende kompetanse i hvordan du programmerer i PHP. I påfølgende kurs
+//skal du bruke PHP til å lage dynamiske nettsteder, men i dette kurset finner
+//du ikke noe om verken HTML eller MySQL. Det kommer senere.
+//
 //Beskrivelse av hvordan du setter opp XAMPP og Komodo...
 //
 //
