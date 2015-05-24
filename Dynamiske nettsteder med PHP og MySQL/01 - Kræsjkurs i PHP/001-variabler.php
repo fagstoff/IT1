9011b08897fd79a720a5dab3d5cce4bdae09be29
bitjungle <devel@bitjungle.com>
2015-05-24 23:09:31 +0200

    Formattering av innledende tekster i skriptene

--- Dynamiske nettsteder med PHP og MySQL/01 - Kræsjkurs i PHP/001-variabler.php
+++ Dynamiske nettsteder med PHP og MySQL/01 - Kræsjkurs i PHP/001-variabler.php
@@ -2,31 +2,33 @@
 //Copyright 2015 BITJUNGLE Rune Mathisen
 //Lisens: https://www.apache.org/licenses/LICENSE-2.0
 
-//Linjer som starter med // er kommentarer som ikke evalueres når programmet kjører.
-//
-//Variabler i PHP kan være av fire ulike datatyper: 
-//		○ Integer - heltall, for eksempel -4 eller 19
-//		○ Float - desimaltall, for eksempel 3,1416
-//		○ String - tekststrenger, for eksempel "Hallo verden!"
-//		○ Boolean - en variabel som bare kan ha to verdier (sant/usant)
-//
-//Variabler i PHP:
-//		○ Starter med et $-tegn
-//		○ Vi tilordner verdi med et likhetstegn: $var = verdi 
-//		○ Vi kan også ha sammensatte datatyper i Array eller Object, det skal vi se på senere
-//
-//Aritmetiske operatorer i PHP:
-//		○ $x + $y      Addisjon
-//		○ $x - $y      Subtraksjon
-//		○ $x * $y      Multiplikasjon
-//		○ $x / $y      Divisjon
-//		○ $x % $y      Modulo-operasjon (rest etter divisjon av heltall)
-//		○ $x ** $y     Eksponent (x opphøyd i y)
-//
-//Vi avslutter alle linjer i PHP med et semikolon.
-//Koden \n betyr ny linje i utskriften fra programmet.
-//For å skrive ut noe fra programmet vårt, bruker vi kommandoen echo.
-//Du lærer mer om bruk av echo i fila 002-streng-1.php.
+
+/*
+ * Variabler i PHP kan være av fire ulike datatyper: 
+ * 		○ Integer - heltall, for eksempel -4 eller 19
+ * 		○ Float - desimaltall, for eksempel 3,1416
+ * 		○ String - tekststrenger, for eksempel "Hallo verden!"
+ * 		○ Boolean - en variabel som bare kan ha to verdier (sant/usant)
+ * 
+ * Variabler i PHP:
+ * 		○ Starter med et $-tegn
+ * 		○ Vi tilordner verdi med et likhetstegn: $var = verdi 
+ * 		○ Vi kan også ha sammensatte datatyper i Array eller Object,
+ * 		  det skal vi se på senere
+ * 
+ * Aritmetiske operatorer i PHP:
+ * 		○ $x + $y      Addisjon
+ * 		○ $x - $y      Subtraksjon
+ * 		○ $x * $y      Multiplikasjon
+ * 		○ $x / $y      Divisjon
+ * 		○ $x % $y      Modulo-operasjon (rest etter divisjon av heltall)
+ * 		○ $x ** $y     Eksponent (x opphøyd i y)
+ * 
+ * Vi avslutter alle linjer i PHP med et semikolon.
+ * Koden \n betyr ny linje i utskriften fra programmet.
+ * For å skrive ut noe fra programmet vårt, bruker vi kommandoen echo.
+ * Du lærer mer om bruk av echo i fila 002-streng-1.php.
+ */
 
 
 //Integer
