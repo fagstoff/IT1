e80e599b4d321e059d869c3a943e7965c848bc9c
bitjungle <devel@bitjungle.com>
2015-05-26 19:01:40 +0200

    To nye filer til MySQL-kurs

---
+++ Dynamiske nettsteder med PHP og MySQL/04 - HTML med PHP og MySQL/001-prepared.php
@@ -0,0 +1,10 @@
+<?php
+//Copyright 2015 BITJUNGLE Rune Mathisen
+//Lisens: https://www.apache.org/licenses/LICENSE-2.0
+
+/*
+ *
+ * http://www.w3schools.com/php/php_mysql_prepared_statements.asp
+ *
+ */
+?>
