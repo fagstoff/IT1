347d6f33823e9c01155cda8bdbecfa130bbde88d
bitjungle <devel@bitjungle.com>
2015-10-30 08:57:50 +0100

    Løsningsforslag til McBergbys-oppgaver

---
+++ Oppgaver/@Løsningsforslag/mcbergbys/11/bestillingsmottak.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="no">
+  <head>
+    <meta charset="utf-8">
+    <title>McBergbys bestillingssystem</title>
+    <link rel="stylesheet" href="stil/mcbergbys-2.css">
+  </head>
+  <body>
+    <pre>
+      <?php
+        print_r($_POST);
+      ?>
+    </pre>
+  </body>
+</html>
