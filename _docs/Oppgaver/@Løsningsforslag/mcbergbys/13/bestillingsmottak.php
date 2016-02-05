cacbbdde5b1b3e77f030cdef786e161b2399ca6e
bitjungle <devel@bitjungle.com>
2016-02-05 08:52:54 +0100

    Oppdaterer løsningsforslag

--- _docs/Oppgaver/@Løsningsforslag/mcbergbys/13/bestillingsmottak.php
+++ _docs/Oppgaver/@Løsningsforslag/mcbergbys/13/bestillingsmottak.php
@@ -18,6 +18,43 @@ if (is_numeric($tlf) && strlen((string)$tlf) == 8) {//Vi har et gyldig nummer
 } else {//Brukeren har ikke tastet inn telefonnummeret riktig
   $tlf_ok = false;
 }
+
+//I koden nedenfor henter vi verdier fra $_POST og setter interne variabler.
+//Vi gjør en sjekk på innholdet i $_POST, og setter eventuelt de interne
+//variablene til tomme verdier dersom det ikke finnes noe relatert innhold i $_POST.
+//Ønsker du å gjøre en grundigere validering av verdiene i $_POST,
+//kan du gjøre det her.
+
+if (isset($_POST['navn'])) {
+  $navn = $_POST['navn'];
+} else {
+  $navn = NULL;
+}
+
+if (isset($_POST['burger'])) {
+  $burger = $_POST['burger'];
+} else {
+  $burger = NULL;
+}
+
+if (isset($_POST['drikke'])) {
+  $drikke = $_POST['drikke'];
+} else {
+  $drikke = NULL;
+}
+
+if (isset($_POST['tilbehør'])) {
+  $tilbehør = $_POST['tilbehør'];
+} else {
+  $tilbehør = NULL;
+}
+
+if (isset($_POST['ekstra'])) {
+  $ekstra = $_POST['ekstra'];
+} else {
+  $ekstra = NULL;
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="no">
@@ -53,29 +90,38 @@ if ($tlf_ok == false) {//Om det er noe galt med tlf-nummeret, gir vi en beskjed 
   echo "Gå tilbake til bestillingsskjemaet og rett det opp ";
   echo "(trykk på tilbakeknappen i nettleseren din).</p>";
 } else {//Dersom telefonnummeret er ok, kjøres koden nedenfor.
-  echo "<p>Tusen takk for din bestilling {$_POST['navn']}.";
+  echo "<p>Tusen takk for din bestilling {$navn}.";
   echo "Vi har registrert bestillingen din på telefonnummer "; 
-  echo "<strong>{$_POST['tlf']}</strong>. "; 
+  echo "<strong>{$tlf}</strong>. "; 
   echo "Vennligst oppgi dette nummeret når du henter bestillingen din.</p>\n";
-  echo "<p>Vi ser at du bestilte en {$_POST['burger']}, det er et godt valg.";
-  if ($_POST['drikke'] == 'vann') {//Brukeren bestilte vann
-    echo "Du bestilte også vann, det er veldig bra!";
-  } else {//Brukeren bestilte noe annet enn vann
-    echo "Du bestilte også {$_POST['drikke']}. ";
-    echo "Jaja. Vann hadde vært et bedre valg, men du vet vel liksom best du da.";
+  echo "<p>";//Begynner et nytt avsnitt her
+  if (isset($burger)) {//Brukeren har bestilt burger
+    echo "Vi ser at du bestilte en {$burger}, det er et godt valg.";
   }
-  echo "Som tilbehør bestilte du {$_POST['tilbehør']}. Greit nok.</p>\n";
-  echo "<p>";
-  //Først tester vi om vi faktisk har en array i variabelen $_POST['ekstra']
+  if (isset($drikke)) {//Brukeren har bestilt drikke
+    if ($drikke == 'vann') {//Brukeren bestilte vann
+      echo "Du bestilte vann, det er veldig bra!";
+    } else {//Brukeren bestilte noe annet enn vann
+      echo "Du bestilte {$drikke}. ";
+      echo "Jaja. Vann hadde vært et bedre valg, men du vet vel liksom best du da.";
+    }
+  }
+  if (isset($tilbehør)) {//Brukeren har bestilt tilbehør
+    echo "Som tilbehør bestilte du {$tilbehør}. Greit nok.";
+  }
+  echo "</p>\n<p>";//Avslutter avsnitt og starter et nytt avsnitt
+  
+  //Nå skal vi se på hva brukeren eventuelt har bestilt av ekstra ting.
+  //Først tester vi om vi faktisk har en array i variabelen $ekstra.
   //Se http://php.net/manual/en/function.is-array.php
-  if(is_array($_POST['ekstra'])) {
+  if(is_array($ekstra)) {
     echo "Vi noterte oss også at du bestilte litt ekstrautstyr til burgeren din. ";
     echo "Her er hva vi har registrert:\n";
     echo "<ul>\n";
     //Nå jobber vi oss steg for steg gjennom hele lista, 
     //og skriver ut innholdet som en punktliste i HTML.
     //Se http://php.net/manual/en/control-structures.foreach.php
-    foreach($_POST['ekstra'] as $e) {
+    foreach($ekstra as $e) {
       echo "<li>{$e}</li>\n";
     }
     echo "</ul></p>\n";
