c83af39be0a2a4297789df5cc0852316f7146519
bitjungle <devel@bitjungle.com>
2018-06-01 09:40:08 +0200

    oppgaver

--- Eksamen/Hjelpefiler/gjestebok.php
+++ Eksamen/Hjelpefiler/gjestebok.php
@@ -1,27 +1,52 @@
 <?php 
-// En superenkel gjestebok
+// En superenkel gjestebok for IT1-elever (med oppgaver)
 // Lisens: Creative Commons BY-SA bitjungle (Rune Mathisen) 2018
-//
-// På forhånd har vi laget denne tabellen i databasen:
-//    CREATE TABLE `Gjestebok` (
-//        `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-//        `tidspunkt` datetime NOT NULL,
-//        `navn` varchar(255) NOT NULL,
-//        `kommentar` text NOT NULL
-//      );
-//
 
-// Dette er informasjonen vi trenger for å koble oss til databasen.
-// Tilpass sånn at det passer til din database.
+/**
+ * Du må lage denne tabellen i en database:
+ *    CREATE TABLE `Gjestebok` (
+ *        `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+ *        `tidspunkt` datetime NOT NULL,
+ *        `navn` varchar(255) NOT NULL,
+ *        `kommentar` text NOT NULL
+ *      );
+ *
+ * I tillegg må di legge inn nødvendig informasjon for å koble seg
+ * til databasen. Fyll ut med korrekt informasjon nedenfor.
+ */
+
 $db_host = 'localhost';
 $db_navn = 'mcbergbys';
 $db_bruker = 'mcbergbys';
 $db_pass = 'Skriv inn passordet ditt her';
 
 /** 
-* Dersom kommentaren eller brukernavnet inneholder uønskede ord, 
-* sensurerer vi det. Legg gjerne inn flere ord i $fyord_liste
-*/
+ * OPPGAVER
+ * ========
+ * 1. Gi gjesteboken en pen layout. Bruk CSS, og gjør eventuelt
+ *    endringer i HTML-koden.
+ * 
+ * 2. Legg inn flere "fy-ord" sånn at uønskede kommentarer sensureres.
+ *    Test for å sjekke at det fungerer.
+ * 
+ * 3. Du skal dele opp "navn" i "fornavn" og "etternavn". For å få 
+ *    til det, må du endre HTML-koden, SQL-koden og definisjonen
+ *    av tabellen i databasen.
+ * 
+ * 4. Gjør nødvendige endringer sånn at kolonna "id" ikke vises 
+ *    i gjesteboka.
+ * 
+ * 5. Bruk alt du kan om HTML, CSS, PHP og SQL. 
+ *    Lag verdens stiligste gjestebok!
+ */
+
+
+
+
+/** 
+ * Dersom kommentaren eller brukernavnet inneholder uønskede ord, 
+ * sensurerer vi det. Legg gjerne inn flere ord i $fyord_liste
+ */
 function sjekk_fyord($tekststreng) {
     $fyord_liste = array('faen', 'helvete', 'dritt');
     $tekststreng_liste = explode(' ', strtolower($tekststreng));
@@ -34,10 +59,10 @@ function sjekk_fyord($tekststreng) {
 }
 
 /**
-* Henter ut verdien fra et bestemt felt i et HTML-skjema. 
-* Her må $feltnavn stemme overens med 'name'-parameteren  
-* til det aktuelle feltet i skjemaet.
-*/
+ * Henter ut verdien fra et bestemt felt i et HTML-skjema. 
+ * Her må $feltnavn stemme overens med 'name'-parameteren  
+ * til det aktuelle feltet i skjemaet.
+ */
 function hent_skjemadata($feltnavn) {
     if (isset($_POST[$feltnavn])&& $_POST[$feltnavn] != '') {
         $verdi = trim($_POST[$feltnavn]);
