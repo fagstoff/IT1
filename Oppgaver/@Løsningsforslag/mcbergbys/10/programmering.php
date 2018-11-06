f5103c0026caf8242b5ea434533ce5c1ffa8e390
bitjungle <devel@bitjungle.com>
2018-11-06 07:02:07 +0100

    Starter på løsningsforslag

---
+++ Oppgaver/@Løsningsforslag/mcbergbys/10/programmering.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Løsningsforslag til programmeringskurs i PHP</title>
+    <style>
+        .kode {
+            white-space: pre;
+            font-family: monospace;
+        }
+    </style>
+</head>
+
+<body>
+
+    <h1>Oppgave 1-1</h1>
+    <div class="kode">
+<?php 
+echo "Hallo verden!\n";
+?>
+    </div>
+
+    <h1>Oppgave 1-2</h1>
+    <div class="kode">
+<?php 
+echo "Jeg heter Rune!\n";
+?>
+    </div>
+
+</body>
+
+</html>
