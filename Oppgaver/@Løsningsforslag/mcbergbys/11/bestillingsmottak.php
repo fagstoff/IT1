477b0f9375653998693b4dc0704349dd4488376f
bitjungle <devel@bitjungle.com>
2018-12-10 09:12:03 +0100

    Oppdatert design

--- Oppgaver/@Løsningsforslag/mcbergbys/11/bestillingsmottak.php
+++ Oppgaver/@Løsningsforslag/mcbergbys/11/bestillingsmottak.php
@@ -1,19 +1,46 @@
 <!DOCTYPE html>
 <html lang="no">
+
 <head>
-	<meta charset="utf-8">
-	<meta name="author" content="bitjungle">
-	<title>Bestillingsmottak</title>
+  <meta charset="utf-8">
+  <meta name="author" content="bitjungle">
+  <title>McBergbys bestillingssystem</title>
   <link rel="stylesheet" href="stiler/mcbergbys-2.css" />
 </head>
+
 <body>
-	<h1>Din bestilling</h1>
-	<pre>
+  <nav>
+    <ul>
+      <li>
+        <div id="logo"><img src="bilder/burger-1487481.svg" alt="McBergbys logo - CC0 midicomp" style="width: 40px;">McBergbys</div>
+      </li>
+      <li>
+        <a href="index.html">Bestilling</a>
+      </li>
+      <li>
+        <a href="om.html">Om McBergbys</a>
+      </li>
+      <li>
+        <a href="hamburgerskolen.html">Burgerskolen</a>
+      </li>
+    </ul>
+  </nav>
+
+  <div class="hoved">
+    <h1>Din bestilling</h1>
+    <pre>
 <code>
 <?php 
  print_r($_POST);
 ?>
 </code>
 </pre>
-</body> 
+
+    <footer>
+      <a href="personvern.html">Personvernerklæring</a>
+    </footer>
+
+  </div><!-- hoved -->
+</body>
+
 </html>
