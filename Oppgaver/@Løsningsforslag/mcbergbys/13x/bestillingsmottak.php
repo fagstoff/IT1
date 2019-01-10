72c1e22bafacd888a818c230bdcdcd7b7b09dad4
bitjungle <devel@bitjungle.com>
2019-01-10 11:28:29 +0100

    forenkler gjennom å fjerne kode som ikke er nødvendig

--- Oppgaver/@Løsningsforslag/mcbergbys/13x/bestillingsmottak.php
+++ Oppgaver/@Løsningsforslag/mcbergbys/13x/bestillingsmottak.php
@@ -1,68 +1,25 @@
 <?php
 //Før vi gir brukeren en bestillingsbekreftelse og lagrer bestillingen 
-//i en database, må vi sjekke om vi har fått all nødvendig informasjon.
-//Vi sier at vi "validerer" informasjonen som brukeren sender fra skjemaet.
-//Foreløpig velger vi å bare sjekke brukerens telefonnummer og navn.
-
+//i en database, må vi sjekke om vi har fått et gyldig telefonnummer.
 //Noen kan finne på å skrive telefonnummeret slik: 999 88 777.
 //Vi fjerner alle mellomrom før vi sjekker om det er et gyldig nummer.
+//Resultatet kopierer vi inn i en ny variabel $tlf
 //Se http://php.net/manual/en/function.str-replace.php
 if (isset($_POST['tlf'])) {
   $tlf = str_replace(" ","", $_POST['tlf']);
 } else {
-  $tlf = NULL;
+  $tlf = null;
 }
-
 //Nå er vi klare for å teste om det er et gyldig telefonnummer.
 //Vi sjekker om det er et tall med 8 siffer. Dersom kravene ikke
 //oppfylles sendes brukeren tilbake til forrige side.
 //Se http://php.net/manual/en/function.is-numeric.php
 //og http://php.net/manual/en/function.strlen.php
-if (is_numeric($tlf) && strlen((string)$tlf) == 8) {//Vi har et gyldig nummer
-  $tlf_ok = true;
-} else {//Brukeren har ikke tastet inn telefonnummeret riktig
-  $tlf_ok = false;
-}
-
-//Inputfeltet "navn" fra HTML-skjemaet er et tekstfelt, og av sikkerhetsmessige
-//grunner er det lurt å gjøre noen sjekker her også. Vi velger å kode alle 
-//spesialtegn i tekststrengen brukeren har tastet inn. Hvis brukeren for eksempel
-//taster inn ">" vil det kodes til "&gt;".
-if (isset($_POST['navn'])) {
-  $navn = htmlspecialchars($_POST['navn']);
+if (is_numeric($tlf) && strlen((string)$tlf) == 8) {
+  $tlf_ok = true;//Vi har et gyldig nummer
 } else {
-  $navn = NULL;
+  $tlf_ok = false;//Telefonnummeret var ugyldig
 }
-
-//I koden nedenfor henter vi resten av verdiene fra $_POST og setter interne variabler.
-//Vi gjør en sjekk på innholdet i $_POST, og setter eventuelt de interne
-//variablene til tomme verdier dersom det ikke finnes noe relatert innhold i $_POST.
-//Ønsker du å gjøre en grundigere validering av disse verdiene i $_POST,
-//kan du gjøre det her.
-if (isset($_POST['burger'])) {
-  $burger = $_POST['burger'];
-} else {
-  $burger = NULL;
-}
-
-if (isset($_POST['drikke'])) {
-  $drikke = $_POST['drikke'];
-} else {
-  $drikke = NULL;
-}
-
-if (isset($_POST['tilbehør'])) {
-  $tilbehør = $_POST['tilbehør'];
-} else {
-  $tilbehør = NULL;
-}
-
-if (isset($_POST['ekstra'])) {
-  $ekstra = $_POST['ekstra'];
-} else {
-  $ekstra = NULL;
-}
-
 ?>
 <!DOCTYPE html>
 <html lang="no">
@@ -100,45 +57,47 @@ if (isset($_POST['ekstra'])) {
   <div class="container" id="hovedomraade">
 
       <h1>Din bestilling</h1>
-      <?php
+<?php
 //Nedenfor skal vi skrive ut en bestillingsbekreftelse.
 if ($tlf_ok == false) {//Om det er noe galt med tlf-nummeret, gir vi en beskjed om det.
   echo "<p>Det er noe galt med telefonnummeret ditt! ";
   echo "Gå tilbake til bestillingsskjemaet og rett det opp ";
   echo "(trykk på tilbakeknappen i nettleseren din).</p>";
-} else {//Dersom telefonnummeret er ok, kjøres koden nedenfor.
-  echo "<p>Tusen takk for din bestilling {$navn}. ";
+} else {//Dersom telefonnummeret er ok, kjøres koden nedenfor.echo "<p>";//Begynner et nytt avsnitt her
+  if (isset($_POST['navn'])) {
+    echo "Tusen takk for din bestilling {$_POST['navn']}. ";
+  }
   echo "Vi har registrert bestillingen din på telefonnummer "; 
   echo "<strong>{$tlf}</strong>. "; 
-  echo "Vennligst oppgi dette nummeret når du henter bestillingen din.</p>\n";
-  echo "<p>";//Begynner et nytt avsnitt her
-  if (isset($burger)) {//Brukeren har bestilt burger
-    echo "Vi ser at du bestilte en {$burger}, det er et godt valg. ";
+  echo "Vennligst oppgi dette nummeret når du henter bestillingen din.";
+  echo "</p>\n<p>";//Begynner et nytt avsnitt her
+  if (isset($_POST['burger'])) {//Brukeren har bestilt burger
+    echo "Vi ser at du bestilte en {$_POST['burger']}, det er et godt valg. ";
   }
-  if (isset($drikke)) {//Brukeren har bestilt drikke
-    if ($drikke == 'vann') {//Brukeren bestilte vann
+  if (isset($_POST['drikke'])) {//Brukeren har bestilt drikke
+    if ($_POST['drikke'] == 'vann') {//Brukeren bestilte vann
       echo "Du bestilte vann, det er veldig bra! ";
     } else {//Brukeren bestilte noe annet enn vann
-      echo "Du bestilte {$drikke}. ";
+      echo "Du bestilte {$_POST['drikke']}. ";
       echo "Jaja. Vann hadde vært et bedre valg, men du vet vel liksom best du da. ";
     }
   }
-  if (isset($tilbehør)) {//Brukeren har bestilt tilbehør
-    echo "Som tilbehør bestilte du {$tilbehør}. Greit nok.";
+  if (isset($_POST['tilbehør'])) {//Brukeren har bestilt tilbehør
+    echo "Som tilbehør bestilte du {$_POST['tilbehør']}. Greit nok.";
   }
   echo "</p>\n<p>";//Avslutter avsnitt og starter et nytt avsnitt
-  
+
   //Nå skal vi se på hva brukeren eventuelt har bestilt av ekstra ting.
   //Først tester vi om vi faktisk har en array i variabelen $ekstra.
   //Se http://php.net/manual/en/function.is-array.php
-  if(is_array($ekstra)) {
+  if(is_array(isset($_POST['ekstra']))) {
     echo "Vi noterte oss også at du bestilte litt ekstrautstyr til burgeren din. ";
     echo "Her er hva vi har registrert:</p>\n";
     echo "<ul>\n";
     //Nå jobber vi oss steg for steg gjennom hele lista, 
     //og skriver ut innholdet som en punktliste i HTML.
     //Se http://php.net/manual/en/control-structures.foreach.php
-    foreach($ekstra as $e) {
+    foreach($_POST['ekstra'] as $e) {
       echo "<li>{$e}</li>\n";
     }
     echo "</ul>\n";
