579683f792c93c7d5d564c125028d65c42438c43
bitjungle <devel@bitjungle.com>
2019-04-30 14:47:50 +0200

    Maler til 1T-prosjekt

---
+++ Oppgaver/@Løsningsforslag/1T-prosjekter/andregrad-backend_mal.php
@@ -0,0 +1,28 @@
+<?php
+//Koden her brukes kun til å teste funksjonene nedenfor.
+//Fjernes når prosjektet er klart til utrulling for brukere.
+
+$_POST['her_er_a'] = 2;
+$_POST['her_er_b'] = -3;
+
+$a = hent_tall('her_er_a');
+$b = hent_tall('her_er_b');
+
+echo beregn($a, $b);
+
+function hent_tall($navn)
+{
+  return $_POST[$navn];
+}
+
+function beregn($a, $b) 
+{
+  return $a*$b; 
+}
+
+function hent_svar() 
+{
+  return "Hallo verden";
+}
+
+?>
---
+++ Oppgaver/@Løsningsforslag/1T-prosjekter/andregrad-frontend_mal.php
@@ -0,0 +1,30 @@
+<?php
+require 'andregrad-backend_mal.php'; 
+?>
+<!DOCTYPE html>
+<html lang="no">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="author" content="bitjungle">
+  <link rel="stylesheet" href="andregrad-stil_mal.css" />
+  <title>Demo som kan brukes til matte-app</title>
+</head>
+
+<body>
+    <h1>Demo som kan brukes til matte-app</h1>
+    <form action="andregrad-frontend_mal.php" method="post">
+      <h2>Skriv inn parametre</h2>
+      <fieldset id="params">
+        <input type="number" name="a" id="her_er_a" placeholder="a" required />
+        <input type="number" name="b" id="her_er_b" placeholder="b" required />
+        <input type="submit" value="&nbsp;Beregn!&nbsp;">
+      </fieldset><!-- param -->
+    </form>
+    <div id="svaromraade">
+      <?php
+      echo hent_svar();
+      ?>
+    </div>
+</body>
+</html>
