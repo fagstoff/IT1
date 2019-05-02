2f94213e6d099167161e898508484006260713c5
bitjungle <devel@bitjungle.com>
2019-05-02 15:31:56 +0200

    forenkler

--- Oppgaver/@Løsningsforslag/1T-prosjekter/andregrad-backend_mal.php
+++ Oppgaver/@Løsningsforslag/1T-prosjekter/andregrad-backend_mal.php
@@ -1,28 +1,20 @@
 <?php
-//Koden her brukes kun til å teste funksjonene nedenfor.
-//Fjernes når prosjektet er klart til utrulling for brukere.
+// De fem kodelinjene nedenfor brukes kun til å teste funksjonene
+// uten å bruke en front-end. Linjene fjernes når prosjektet er 
+// klart til utrulling for brukere.
 
-$_POST['her_er_a'] = 2;
-$_POST['her_er_b'] = -3;
-
+$_POST['her_er_a'] = 2; //Simulerer verdier fra et HTML-skjema
+$_POST['her_er_b'] = -3;//Simulerer verdier fra et HTML-skjema
 $a = hent_tall('her_er_a');
 $b = hent_tall('her_er_b');
+echo funksjonstest($a, $b);
 
-echo beregn($a, $b);
-
-function hent_tall($navn)
-{
+function hent_tall($navn) {
   return $_POST[$navn];
 }
 
-function beregn($a, $b) 
-{
-  return $a*$b; 
-}
-
-function funksjonstest() 
-{
-  return "Hallo verden";
+function funksjonstest($a, $b) {
+  return $a * $b;
 }
 
 ?>
